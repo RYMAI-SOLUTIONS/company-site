a604074f72d4af3e90284c384370371eb85b1644
RYMAI-SOLUTIONS <RYMAI-SOLUTIONS@users.noreply.github.com>
2026-07-05 13:56:12 +0200

    Move JS assets

--- assets/js/intro-birds.js
+++ assets/js/intro-birds.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    AOS.init();
-
     if (!window.THREE || !window.VANTA || !window.VANTA.BIRDS) {
         console.error("Vanta.BIRDS failed to initialize: THREE or VANTA.BIRDS not found.");
         return;
     }
+
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const backgroundColor = isDarkMode ? 0x0d0d0d : 0xffffff;
+
+    // const heroContainer = document.querySelector('.hero');
+    // const introContainer = document.querySelector('.intro');
+    // if (introContainer && heroContainer) {
+    //     introContainer.style.marginTop =  (heroContainer.offsetHeight + 40) + 'px';
+    // }
 
     window.VANTA.BIRDS({
         el: ".hero",
@@ -16,7 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
         minWidth: 200.00,
         scale: 1.0,
         scaleMobile: 1.0,
-        backgroundColor: 0x0d0d0d,
+        // backgroundColor: backgroundColor,
+        backgroundColor: 0x000000,
         //backgroundColor: 0x000000,
         color1: 0xc18464,
         color2: 0x274394,
